a1a489f6df5d3729362f3932b74bc5e22211b792
cassie <cleder@cub.uca.edu>
2024-08-26 15:45:52 -0500

    Initial commit

---
+++ src/Greeter.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Greeter
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        in.nextLine();
+        System.out.println("Hello world!");
+    }
+}
